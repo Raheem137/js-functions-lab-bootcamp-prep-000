b3402a46abb15e76ce7008791bfb52cfcab3dde4
Raheem <raheemtheallstar137@yahoo.com>
2018-10-27 15:16:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // write your code below!
+function happyHolidays(){
+  var greet = "Happy holidasy!";
+  return greet;
+}
+
+function happyHolidaysTo(){
+  
+}
+
+function happyHolidayTo(){
+  
+}
